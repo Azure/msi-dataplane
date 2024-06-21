fc8cba930642d3bb5bcd25c4662006f72fd3d75f
Nicolas Ontiveros <nicolas.ontiveros@microsoft.com>
2024-06-21 14:00:46 -0700

    Save work

--- pkg/dataplane/stub.go
+++ pkg/dataplane/stub.go
@@ -2,44 +2,113 @@ package dataplane
 
 import (
 	"bytes"
-	"context"
-	"fmt"
-	"io/ioutil"
+	"hash/fnv"
+	"io"
 	"net/http"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/msi-dataplane/internal/swagger"
-	"github.com/Azure/msi-dataplane/internal/test"
 )
 
+// Hash table to store identities for dataplane
+// Key is a hash of certain fields in the credentials object
+type identityHashMap map[uint64]*CredentialsObject
+
+// Stub is a transport that mocks the Managed Identity Dataplane API
 // TODO - add support for system-assigned managed identities
-type stubTransporter struct {
+type stub struct {
+	// Key is a hash of the resource IDs
+	userAssignedIdentities map[uint64]*CredentialsObject
 }
 
-func (tp stubTransporter) Do(req *http.Request) (*http.Response, error) {
-	fmt.Printf("Request: %v\n", *req)
-
-	// Read the request body
-	bodyBytes, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		return nil, err // Handle error
+func NewStub(creds []*CredentialsObject) *stub {
+	userAssignedIdentities := make(map[uint64]*CredentialsObject)
+	for _, identity := range creds {
+		if identity != nil && identity.IsUserAssigned() {
+			resourceIDs := make([]string, 0)
+			for _, uaMSI := range identity.ExplicitIdentities {
+				if uaMSI != nil && uaMSI.ResourceID != nil {
+					resourceIDs = append(resourceIDs, *uaMSI.ResourceID)
+				}
+			}
+			if len(resourceIDs) != 0 {
+				hash := hashResourceIDs(resourceIDs)
+				userAssignedIdentities[hash] = identity
+			}
+		}
 	}
-	// Replace the request body so it can be read again
-	req.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-	// Convert the body to a string and print
-	bodyString := string(bodyBytes)
-	fmt.Printf("Request body: %s\n", bodyString)
-
-	return &http.Response{}, nil
+	return &stub{userAssignedIdentities: userAssignedIdentities}
 }
 
-type ManagedIdentityStub struct {
-	swaggerClient msiClient
+// Implement transport interface
+// Per MSI team's documentation, POST is for user-assigned MSI and GET is for system-assigned MSI
+// https://eng.ms/docs/products/arm/rbac/managed_identities/msionboardinguserassigned
+func (s stub) Do(req *http.Request) (*http.Response, error) {
+	var response *http.Response
+	var err error
+
+	switch req.Method {
+	case http.MethodPost:
+		// User-assigned managed identities request
+		response, err = s.post(req)
+	default:
+		response = &http.Response{
+			StatusCode: http.StatusNotImplemented,
+			Body:       io.NopCloser(bytes.NewBufferString("")),
+		}
+	}
+
+	return response, err
 }
 
+func (s stub) post(req *http.Request) (*http.Response, error) {
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return &http.Response{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	credRequestDefinition := &swagger.CredRequestDefinition{}
+	if err := credRequestDefinition.UnmarshalJSON(bodyBytes); err != nil {
+		return &http.Response{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	identityList := credRequestDefinition.IdentityIDs
+	resourceIds := make([]string, 0)
+	for _, identity := range identityList {
+		if identity != nil {
+			resourceIds = append(resourceIds, *identity)
+		}
+	}
+
+	hash := hashResourceIDs(resourceIds)
+	creds, ok := s.userAssignedIdentities[hash]
+	if !ok {
+		return &http.Response{StatusCode: http.StatusNotFound}, nil
+	}
+
+	// Marshal the credentials object into the body
+	credBytes, err := creds.MarshalJSON()
+	if err != nil {
+		return &http.Response{StatusCode: http.StatusInternalServerError}, err
+	}
+	response := &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(bytes.NewBuffer(credBytes)),
+	}
+
+	return response, nil
+}
+
+func hashResourceIDs(resourceIDs []string) uint64 {
+	h := fnv.New64a()
+
+	for _, id := range resourceIDs {
+		h.Write([]byte(id))
+	}
+
+	return h.Sum64()
+}
+
+/*
 func NewStub(cloud string) (*ManagedIdentityStub, error) {
 	plOpts := runtime.PipelineOptions{
 		PerCall: []policy.Policy{
@@ -72,3 +141,4 @@ func (s *ManagedIdentityStub) GetUserAssignedIdentities(ctx context.Context, req
 
 	return nil, err
 }
+*/
